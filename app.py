60724c6a6ef78c15544b15103a49e91eb37f8434
pixls <tha.pixls@gmail.com>
2022-03-15 17:53:49 -0400

    three initial routes working
    no error handling for incomplete post requests, but posts without image are handled properly

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask app for Cupcakes"""
 
-from flask import Flask, jsonify, request, redirect, render_template
+from flask import Flask, jsonify, request
 from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ connect_db(app)
 
 
 def serialize(cupcake):
-    """return a serialized version of cupcake info"""
+    """return a dictionary of a given cupcake"""
 
     return {
         "id": cupcake.id,
@@ -26,7 +26,37 @@ def serialize(cupcake):
 @app.route("/api/cupcakes/")
 def all_cupcakes():
     """return list of all cupcakes"""
+
     cupcakes = Cupcake.query.all()
     serialized = [serialize(c) for c in cupcakes]
 
     return jsonify(cupcakes=serialized)
+
+
+@app.route("/api/cupcakes/<int:cupcake_id>/")
+def cupcake_details(cupcake_id):
+    """return information about one specific cupcake by id"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = serialize(cupcake)
+
+    return jsonify(cupcake=serialized)
+
+
+@app.route("/api/cupcakes/", methods=["POST"])
+def add_cupcake():
+    """adds a new cupcake to cupcakes table"""
+
+    flavor = request.json.get("flavor")
+    size = request.json.get("size")
+    rating = request.json.get("rating")
+    image = request.json.get("image")
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = serialize(new_cupcake)
+
+    return (jsonify(cupcake=serialized), 201)
