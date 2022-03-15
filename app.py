0a909ed99fd04eb5909e15508827af2951ba0cf3
pixls <tha.pixls@gmail.com>
2022-03-15 17:23:00 -0400

    added first api route and serialize function
    all_cupcakes returns list of all cupcakes, serialize formats cupcake data into dict

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 """Flask app for Cupcakes"""
 
-from flask import Flask, request, redirect, render_template
+from flask import Flask, jsonify, request, redirect, render_template
 from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///cupcakes"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 
 connect_db(app)
+
+
+def serialize(cupcake):
+    """return a serialized version of cupcake info"""
+
+    return {
+        "id": cupcake.id,
+        "flavor": cupcake.flavor,
+        "size": cupcake.size,
+        "rating": cupcake.rating,
+        "image": cupcake.image,
+    }
+
+
+@app.route("/api/cupcakes/")
+def all_cupcakes():
+    """return list of all cupcakes"""
+    cupcakes = Cupcake.query.all()
+    serialized = [serialize(c) for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
--- models.py
+++ models.py
@@ -4,11 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def connect_db(app):
     """connect to database"""
 
     db.app = app
     db.init_app(app)
+
 
 class Cupcake(db.Model):
     """Cupcakes!"""
@@ -19,4 +21,6 @@ class Cupcake(db.Model):
     flavor = db.Column(db.String, nullable=False)
     size = db.Column(db.String, nullable=False)
     rating = db.Column(db.Float, nullable=False)
-    image = db.Column(db.String, nullable=False, default="https://tinyurl.com/demo-cupcake")
+    image = db.Column(
+        db.String, nullable=False, default="https://tinyurl.com/demo-cupcake"
+    )
